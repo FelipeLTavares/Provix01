6144cb800bffdd43ce65f5f117060024e5ceb400
FelipeLTavares <felipelimatavares96@gmail.com>
2023-02-12 19:33:10 -0300

    Segundo commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Inicio from "./pages/Inicio";
 import Empresa from "./pages/Empresa";
+import Contato from "./pages/Contato";
 
 const router = createBrowserRouter([
   { path: "/", element: <Inicio /> },
   { path: "/empresa", element: <Empresa /> },
+  { path: "/empresa", element: <Contato /> },
 ]);
 
 function App() {
