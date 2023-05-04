beab71a3e1abe396c785ee1a2497d102fb8d4278
gustavonunes0 <gustavonoroes@outlook.com>
2023-05-04 18:25:01 -0300

    changes

--- src/shared/Carrossel/index.tsx
+++ src/shared/Carrossel/index.tsx
@@ -15,7 +15,6 @@ import logo12 from '../../assets/IMG_0615.png'
 import logo13 from '../../assets/IMG_0616.webp'
 import logo14 from '../../assets/IMG_0617.png'
 import logo15 from '../../assets/IMG_0619.png'
-import logo16 from '../../assets/IMG_0620.png'
 import logo17 from '../../assets/IMG_0621.png'
 import logo18 from '../../assets/IMG_0622.png'
 
@@ -23,7 +22,7 @@ interface Image {
     [key: string]: any;
 }
 
-const images: Image = { logo1, logo2, logo3, logo4, logo5, logo7, logo8, logo9, logo10, logo11, logo12, logo13, logo14, logo15, logo16, logo17, logo18 }
+const images: Image = { logo1, logo2, logo3, logo4, logo5, logo7, logo8, logo9, logo10, logo11, logo12, logo13, logo14, logo15, logo17, logo18 }
 
 const repeatedImages = [...Object.values(images),...Object.values(images),...Object.values(images),...Object.values(images),...Object.values(images),...Object.values(images),...Object.values(images),...Object.values(images), ...Object.values(images), ...Object.values(images), ...Object.values(images), ...Object.values(images), ...Object.values(images), ...Object.values(images), ...Object.values(images), ...Object.values(images), ...Object.values(images), ...Object.values(images), ...Object.values(images), ...Object.values(images), ...Object.values(images)];
 
