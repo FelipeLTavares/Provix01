f03d2e4f741a03aa893ae3a33e0f4826cf770d90
gustavonunes0 <gustavonoroes@outlook.com>
2023-05-19 14:36:09 -0300

    changes

--- src/pages/Inicio/Main/index.tsx
+++ src/pages/Inicio/Main/index.tsx
@@ -16,16 +16,16 @@ const Main = () => {
         <div id="MCSubtitle">É O QUE ENTREGAMOS</div>
 
         <div id="MCLeftContent">
-          <p style={{marginBottom: '-4%', fontSize: '1.2rem'}}>
+          <p style={{marginBottom: '-1%', fontSize: '1.2rem'}}>
             Criada em 2011 pela empresária cearense Francisca Gardia, a Provix Distribuidora é uma empresa focada na distribuição de produtos alimentícios, higiene pessoal, limpeza, entre outros. 
           </p>
-          <p style={{marginBottom: '-4%', fontSize: '1.2rem'}}>
+          <p style={{marginBottom: '-1%', fontSize: '1.2rem'}}>
             Atualmente atende todos os municípios do Ceará, comercializando um mix variado de produtos. 
           </p>
-          <p style={{marginBottom: '-4%', fontSize: '1.2rem'}}>
+          <p style={{marginBottom: '-1%', fontSize: '1.2rem'}}>
             Investe permanentemente em tecnologia, mantém relações comerciais de confiança por meio do cumprimento de todos os compromissos com clientes e fornecedores. Leva em conta o respeito, a ética, a humildade e a disciplina em cada negócio.
           </p>
-          <p style={{marginBottom: '-4%', fontSize: '1.2rem'}}>
+          <p style={{marginBottom: '-1%', fontSize: '1.2rem'}}>
             Nesses anos de atividade empresarial, a Provix tem passado por processos de melhoria contínua de suas atividades, buscando se adaptar ao mercado cada vez mais competitivo, evoluindo na atuação e identificação no seu segmento.   
           </p>
           <p style={{fontSize: '1.2rem'}}>
